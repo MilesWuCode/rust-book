be7c9ca024257dd4d8c98dce8c261971bc08b0c0
Miles Wu <slion0725@gmail.com>
2024-01-20 14:48:15 +0000

    upd

--- 02.hello_cargo/target/debug/deps/hello_cargo-9add631b3a00e272.d
+++
@@ -1,5 +0,0 @@
-/home/miles/github/rust-book/02.hello_cargo/target/debug/deps/libhello_cargo-9add631b3a00e272.rmeta: src/main.rs
-
-/home/miles/github/rust-book/02.hello_cargo/target/debug/deps/hello_cargo-9add631b3a00e272.d: src/main.rs
-
-src/main.rs:
--- 02.hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/miles/github/rust-book/02.hello_cargo/target/debug/hello_cargo: /home/miles/github/rust-book/02.hello_cargo/src/main.rs
